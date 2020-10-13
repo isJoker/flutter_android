c288d2dee04789a9a2979a74f4999c4bcf8fff72
wanjiacheng <wanjiacheng@webuy.ai>
2020-10-13 14:04:05 +0800

    [mod]项目模块UI调整

--- lib/project/project_item/view.dart
+++ lib/project/project_item/view.dart
@@ -1,80 +1,80 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_android/common/util/ui_adapter.dart';
-import 'package:flutter_android/project/project_detail/action.dart';
 
 import 'action.dart';
 import 'state.dart';
 
-Widget buildView(ProjectItemState state, Dispatch dispatch, ViewService viewService) {
+Widget buildView(
+    ProjectItemState state, Dispatch dispatch, ViewService viewService) {
   return Card(
     shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(setWidth(20))
-    ),
+        borderRadius: BorderRadius.circular(setWidth(20))),
     child: InkWell(
       borderRadius: BorderRadius.circular(setWidth(20)),
       child: Row(
         children: [
           Expanded(
               child: Container(
-                margin: EdgeInsets.all(setWidth(20)),
-                height: setWidth(350),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    // 标题
-                    Expanded(
-                      flex: 1,
-                      child: Text(
-                        state.itemData.title,
-                        style: TextStyle(fontSize: setSp(34)),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
+            margin: EdgeInsets.all(setWidth(20)),
+            height: setWidth(350),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                // 标题
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      state.itemData.title,
+                      style:
+                          TextStyle(fontSize: setSp(34), color: Colors.black),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
                     ),
-                    // 描述
-                    Expanded(
-                        flex: 5,
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            state.itemData.desc,
-                            style: TextStyle(fontSize: setSp(28)),
-                            maxLines: 4,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        )),
-                    Expanded(
-                      flex: 2,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          //作者
-                          Row(
-                            children: <Widget>[
-                              Text("作者："),
-                              Expanded(
-                                child: Text(state.itemData.author,
-                                    style: TextStyle(color: Colors.blue)),
-                              )
-                            ],
-                          ),
-                          //时间
-                          Row(
-                            children: <Widget>[
-                              Text("时间："),
-                              Expanded(
-                                child: Text(state.itemData.niceDate,
-                                    style: TextStyle(color: Colors.blue)),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              )),
+                // 描述
+                Expanded(
+                    flex: 5,
+                    child: Container(
+                      margin: EdgeInsets.only(top: setWidth(20)),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        state.itemData.desc,
+                        style: TextStyle(
+                            fontSize: setSp(28), color: Colors.black87),
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.left,
+                      ),
+                    )),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      //作者
+                      Container(
+                        child: Text("作者：${state.itemData.author}",
+                            style: TextStyle(color: Colors.black54)),
+                        alignment: Alignment.centerLeft,
+                      ),
+                      //时间
+                      Container(
+                        child: Text("时间：${state.itemData.niceDate}",
+                            style: TextStyle(color: Colors.black45)),
+                        alignment: Alignment.centerLeft,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
           Container(
             margin: EdgeInsets.all(setWidth(20)),
             width: setWidth(180),
@@ -86,7 +86,7 @@ Widget buildView(ProjectItemState state, Dispatch dispatch, ViewService viewServ
           ),
         ],
       ),
-      onTap: (){
+      onTap: () {
         dispatch(ProjectItemActionCreator.onProjectItemClick(state));
       },
     ),
